ca808f490f735c449d00db29540a80fb396453b2
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2023-01-05 22:10:00 +0600

    .

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,10 +1,14 @@
-import { Admin, Resource, CustomRoutes } from "react-admin";
+import { Admin, Resource, CustomRoutes, Authenticated } from "react-admin";
 import { Route } from "react-router-dom";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./users";
 import { authProvider } from './authProvider';
-import Settings from './Settings';
+import Settings from './settings';
 import { defaultTheme } from 'react-admin';
+import UserIcon from '@mui/icons-material/People';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const theme = {
     ...defaultTheme,
@@ -13,13 +17,12 @@ const theme = {
     },
 };
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-
 const App = () => (
+
    <Admin theme={theme} authProvider={authProvider} dataProvider={dataProvider}>
-     <Resource name="settings" list={UserList}/>
-     <Resource name="users" list={UserList} />
+     <Resource name="settings" list={<Authenticated requireAuth><Settings userID="1234" /></Authenticated>} icon={SettingsIcon} options={{ label: 'Настройки' }}/>
+     <Resource name="users" list={UserList} icon={UserIcon} options={{ label: 'Пользователи (тест)' }}/>
    </Admin>
-  );
+);
 
 export default App;
